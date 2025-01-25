994082dc7f4705c5b71d1198b75cc46adb102fa9
Samar Khan <samarik2004@gmail.com>
2025-01-25 14:46:52 +0530

    Make the Player class

--- ascii/src/Player.java
+++ ascii/src/Player.java
@@ -1,7 +1,27 @@
 import java.util.ArrayList;
 
 public class Player {
-private String character;
-private int health;
-private ArrayList<String> inventory;
+    private String name;
+    private String character;
+    private int health;
+    private ArrayList<String> inventory;
+
+    public void setCharacter(String character, String name) {
+        this.character = character;
+        this.name = name;
+    }
+
+    public void increaseHealth() {
+        if (this.health < 5) {
+            this.health++;
+        }
+    }
+
+    public void addItem(String item) {
+        this.inventory.add(item);
+    }
+
+    public String toString() {
+        return String.format("Name: %s\nHealth: %d\nRole: %s", this.name, this.health, this.character);
+    }
 }
